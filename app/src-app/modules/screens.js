51d92d5abe6183cba4214f9618c6f4893f7bda0d
joaoWillamy <willamyse@gmail.com>
2018-05-19 18:37:58 -0300

    screens

--- app/src-app/modules/screens.js
+++ app/src-app/modules/screens.js
@@ -3,7 +3,14 @@ import { Navigation } from 'react-native-navigation';
 import Login from './user/Login.screen';
 import MyCards from './shared/MyCards.screen';
 
+// user one
 export function registerScreens(store, Provider) {
 	Navigation.registerComponent('Login', () => Login, store, Provider);
 	Navigation.registerComponent('MyCards', () => MyCards, store, Provider);
+}
+
+//  user admin
+export function registerScreensAdmin(store, Provider) {
+	// Navigation.registerComponent('Login', () => Login, store, Provider);
+	// Navigation.registerComponent('MyCards', () => MyCards, store, Provider);
 }
